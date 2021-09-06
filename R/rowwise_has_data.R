ad11ecc4719c0f2bc95c67c5bbad8f0a5bf286d8
mattansb <35330040+mattansb@users.noreply.github.com>
2021-09-06 10:15:34 +0300

    new: has_any_data

---
+++ R/rowwise_has_data.R
@@ -0,0 +1,48 @@
+#' Test rowwise if each row is missing / has all data in select columns
+#'
+#' @inheritParams dplyr::select
+#' @param .name Name of the new column with the logical index.
+#'
+#' @examples
+#' data(mtcars)
+#'
+#' mtcars[1, 1] <- NA
+#' mtcars[2, ] <- NA
+#'
+#' mtcars[1:3, 1:3] |>
+#'   has_any_data(mpg:disp, .name = "has_any") |>
+#'   has_all_data(mpg:disp, .name = "has_all") |>
+#'   missing_any_data(mpg:disp, .name = "missing_any") |>
+#'   missing_all_data(mpg:disp, .name = "missing_all")
+#'
+#' @export
+has_any_data <- function(.data, ..., .name) {
+  .check_namespace("dplyr")
+  .data[[.name]] <- apply(!is.na(dplyr::select(.data, ...)), 1, any)
+  .data
+}
+
+#' @export
+#' @rdname has_any_data
+has_all_data <- function(.data, ..., .name) {
+  .check_namespace("dplyr")
+  .data[[.name]] <- apply(dplyr::select(.data, ...), 1, Negate(anyNA))
+  .data
+}
+
+#' @export
+#' @rdname has_any_data
+missing_any_data <- function(.data, ..., .name) {
+  .data <- has_all_data(.data, ..., .name = .name)
+  .data[[.name]] <- !.data[[.name]]
+  .data
+}
+
+#' @export
+#' @rdname has_any_data
+missing_all_data <- function(.data, ..., .name) {
+  .data1 <- has_all_data(.data, ..., .name = .name)
+  .data2 <- has_any_data(.data, ..., .name = .name)
+  .data1[[.name]] <- !.data1[[.name]] & !.data2[[.name]]
+  .data1
+}
