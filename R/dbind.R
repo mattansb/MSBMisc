7077cb222a5f7b0ecf34f454933f7d3738afb525
Mattan S. Ben-Shachar <matanshm@post.bgu.ac.il>
2022-01-05 09:44:09 +0200

    update dbind

--- R/dbind.R
+++ R/dbind.R
@@ -1,30 +1,35 @@
 #' Bind matrices diagonally
 #'
 #' @param ... Matrices.
-#' @param .fill Value to fill the off-"diagonal" values.
+#' @param .fill Value to fill the off-"diagonal" values. If `NULL`, the value is
+#'   the default value of the inputs' mode.
 #'
 #'
 #' @examples
-
-#' M1 <- matrix(1:9, 3, 3)
-#' M2 <- matrix(10:15, 2, 2)
 #'
+#' M1 <- matrix(1:8, 2, 4)
+#' M2 <- matrix(9:14, 2, 2)
 #' dbind(M1, M2)
 #' dbind(M1, M2, .fill = NA)
 #'
 #'
-#'
-#' M1 <- matrix(letters[1:9], 3, 3)
-#' M2 <- matrix(LETTERS[10:15], 2, 2)
-#'
+#' M1 <- matrix(letters[1:4], 2, 2)
+#' M2 <- matrix(LETTERS[5:10], 3, 2)
+#' dbind(M1, M2)
 #' dbind(M1, M2, .fill = "Banana")
-#' dbind(M1, M2, .fill = NA)
+#'
+#'
+#' M1 <- matrix(TRUE, 2, 3)
+#' M2 <- matrix(NA, 3, 4)
+#' dbind(M1, M2)
+#'
 #'
 #' @export
-dbind <- function(..., .fill = 0) {
+dbind <- function(..., .fill = NULL) {
   mats <- list(...) |> lapply(as.matrix)
 
-  stopifnot("All inputs must be matrices." = all(sapply(mats, is.matrix)))
+  stopifnot("All inputs must be matrices." = all(sapply(mats, is.matrix)),
+            "All inputs must be of same mode." = length(unique(sapply(mats, mode))) == 1L)
 
   n.row <- sapply(mats, nrow)
   n.col <- sapply(mats, ncol)
@@ -35,6 +40,7 @@ dbind <- function(..., .fill = 0) {
   start.rows <- end.rows - n.row + 1
   start.cols <- end.cols - n.col + 1
 
+  if (is.null(.fill)) .fill <- vector(mode = mode(mats[[1]]), length = 1)
   out <- matrix(.fill, nrow = tail(end.rows, 1), ncol = tail(end.cols, 1),
                 dimnames = list(1:sum(n.row), 1:sum(n.col)))
   for (m in seq_along(mats)) {
